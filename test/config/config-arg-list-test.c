32e93d1ca942a0738aaf768d0e91fee1d0ae0092
Gergely Koloszar <gergely.koloszar@gmail.com>
2024-09-18 19:56:45 +0200

    Add argument list tests

--- test/config/config-arg-list-test.c
+++ test/config/config-arg-list-test.c
@@ -90,12 +90,117 @@ void destroy_argument_list_test() {
     TEST_ASSERT_NULL(target1);
     TEST_ASSERT_NULL(target2);
     TEST_ASSERT_NULL(target3);
-    
 }
     
-void add_argument_test();
+void add_argument_test_add_first() {
+    ArgumentList alist = create_argument_list();
 
-void find_argument_test();
+    int ret = add_argument(&alist, create_argument("key1", Integer));
 
-void remove_argument_test();
+    TEST_ASSERT_EQUAL(0, ret);
+    TEST_ASSERT_EQUAL_STRING("key1", alist.list->argument.key);
+    TEST_ASSERT_EQUAL_INT(Integer, alist.list->argument.key);
+}
 
+void add_argument_test_add_more() {
+    ArgumentList alist = create_argument_list();
+
+    int ret1 = add_argument(&alist, create_argument("key1", Integer));
+    int ret2 = add_argument(&alist, create_argument("key2", Float));
+    int ret3 = add_argument(&alist, create_argument("key3", String));
+
+    TEST_ASSERT_EQUAL(0, ret1);
+    TEST_ASSERT_EQUAL(0, ret2);
+    TEST_ASSERT_EQUAL(0, ret3);
+
+    TEST_ASSERT_EQUAL_STRING("key1", alist.list->argument.key);
+    TEST_ASSERT_EQUAL_INT(Integer, alist.list->argument.key);
+
+    TEST_ASSERT_EQUAL_STRING("key2", alist.list->next->argument.key);
+    TEST_ASSERT_EQUAL_INT(Integer, alist.list->next->argument.key);
+
+    TEST_ASSERT_EQUAL_STRING("key1", alist.list->next->next->argument.key);
+    TEST_ASSERT_EQUAL_INT(Integer, alist.list->next->next->argument.key);
+}
+
+void find_argument_test_target_found() {
+    ArgumentList alist = create_argument_list();
+
+    add_argument(&alist, create_argument("key1", Integer));
+    add_argument(&alist, create_argument("key2", Float));
+    add_argument(&alist, create_argument("key3", String));
+
+    const Argument* res1;
+    int ret1 = find_argument(&alist, "key1", &res1);
+
+    TEST_ASSERT_EQUAL_INT(0, ret1);
+    TEST_ASSERT_EQUAL_STRING("key1", res1->key);
+    TEST_ASSERT_EQUAL_INT(Integer, res1->type);
+
+    const Argument* res2;
+    int ret2 = find_argument(&alist, "key2", &res2);
+
+    TEST_ASSERT_EQUAL_INT(0, ret2);
+    TEST_ASSERT_EQUAL_STRING("key2", res2->key);
+    TEST_ASSERT_EQUAL_INT(Float, res2->type);
+
+    const Argument* res3;
+    int ret3 = find_argument(&alist, "key3", &res3);
+
+    TEST_ASSERT_EQUAL_INT(0, ret3);
+    TEST_ASSERT_EQUAL_STRING("key3", res3->key);
+    TEST_ASSERT_EQUAL_INT(String, res3->type);
+}
+
+void find_argument_test_target_not_found() {
+    ArgumentList alist = create_argument_list();
+
+    add_argument(&alist, create_argument("key1", Integer));
+    add_argument(&alist, create_argument("key2", Float));
+    add_argument(&alist, create_argument("key3", String));
+
+    const Argument* res1 = NULL;
+    int ret = find_argument(&alist, "nonexistent", &res1);
+
+    TEST_ASSERT_EQUAL(-1, ret);
+    TEST_ASSERT_EQUAL_INT(NULL, res1);
+}
+
+void remove_argument_test_target_found() {
+    ArgumentList alist = create_argument_list();
+
+    add_argument(&alist, create_argument("key1", Integer));
+    add_argument(&alist, create_argument("key2", Float));
+    add_argument(&alist, create_argument("key3", String));
+
+    int res = remove_argument(&alist, "key2");
+
+    TEST_ASSERT_EQUAL(0, res);
+
+    TEST_ASSERT_EQUAL_STRING("key1", alist.list->argument.key);
+    TEST_ASSERT_EQUAL_INT(Integer, alist.list->argument.type);
+
+    TEST_ASSERT_EQUAL_STRING("key3", alist.list->next->argument.key);
+    TEST_ASSERT_EQUAL_INT(String, alist.list->next->argument.type);
+}
+
+void remove_argument_test_target_not_found() {
+    ArgumentList alist = create_argument_list();
+
+    add_argument(&alist, create_argument("key1", Integer));
+    add_argument(&alist, create_argument("key2", Float));
+    add_argument(&alist, create_argument("key3", String));
+
+    int res = remove_argument(&alist, "nonexistent");
+
+    TEST_ASSERT_EQUAL(-1, res);
+
+    TEST_ASSERT_EQUAL_STRING("key1", alist.list->argument.key);
+    TEST_ASSERT_EQUAL_INT(Integer, alist.list->argument.type);
+
+    TEST_ASSERT_EQUAL_STRING("key2", alist.list->next->argument.key);
+    TEST_ASSERT_EQUAL_INT(String, alist.list->next->argument.type);
+
+    TEST_ASSERT_EQUAL_STRING("key3", alist.list->next->next->argument.key);
+    TEST_ASSERT_EQUAL_INT(String, alist.list->next->next->argument.type);
+}
