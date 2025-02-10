5e9eac9158932f12b38d3ea80664df6cfae802b0
Gergely Koloszar <gergely.koloszar@gmail.com>
2025-02-10 22:05:59 +0100

    Add documentation comments to commandline argument parser

--- src/config/cmdline-parser.h
+++ src/config/cmdline-parser.h
@@ -3,6 +3,18 @@
 
 #include "configuration.h"
 
+/**
+ * @brief Parse arguments from command line and override configuration values.
+ *
+ * This function gets the argument list, and then sweeps it. For every option found,
+ * the given configuration struct is updated accordingly.
+ *
+ * @param argc - const int argument counter.
+ * @param argv - const char** string array of the given arguments.
+ * @param conf - Configuration* pointer to the configuration structure.
+ *
+ * @return Upon success, 0 is returned. On error, a non-zero number is returned.
+ */
 int parse_cmdline_arguments(const int argc, const char *argv[], Configuration *conf);
     
 #endif    
