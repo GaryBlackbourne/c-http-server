deb4db6f74aa0e88f343cd0533a5904bec420111
Gergely Koloszar <gergely.koloszar@gmail.com>
2025-02-10 21:28:50 +0100

    Add workerpool frame code

--- src/workerpool/workerpool.h
+++ src/workerpool/workerpool.h
@@ -1,14 +1,17 @@
 #ifndef _BALU_WORKERPOOL_H
 #define _BALU_WORKERPOOL_H
 
-#include "connection.h"
+#include <stdint.h>
+#include "configuration.h"
 
 typedef struct {
-    Connection *connections;
-    uint16_t max_connections;
+    // workers
+    uint16_t max_workers;
 } Workerpool;
 
+int workerpool_init(Workerpool* workerpool, const Configuration* config);
 
+int workerpool_destroy(Workerpool* workerpool);
 
 
 #endif
