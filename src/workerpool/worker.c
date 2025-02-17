e1e1d3a1247f6eca086502d584ef1b102bd2bcbe
Gergely Koloszar <gergely.koloszar@gmail.com>
2025-02-17 18:55:14 +0100

    Add worker function to struct, and implement init

--- src/workerpool/worker.c
+++ src/workerpool/worker.c
@@ -1,3 +1,28 @@
+#include <assert.h>
+#include <pthread.h>
+
 #include "worker.h"
+#include "fifo.h"
 
+int worker_start(Worker* worker) {
+    assert(worker != NULL);
+    worker->function(NULL);
+}
 
+int worker_init(Worker* worker, const Fifo* job_queue) {
+    assert(worker != NULL);
+    assert(job_queue != NULL);
+
+    // init pthread handler
+
+    // init thread function
+    worker->function = worker_thread_function;
+
+    worker->thread_argument.job_queue = job_queue;
+
+    return 0;
+}
+
+int worker_destroy(Worker* worker) {
+    
+}
--- src/workerpool/worker.h
+++ src/workerpool/worker.h
@@ -3,16 +3,21 @@
 
 #include <stdint.h>
 #include <pthread.h>
+
 #include "connection.h"
+#include "fifo.h"
 
 typedef struct {
     pthread_t handler;
-    void (*worker_function) (void* vp);
-    Connection connection;
+    void (*function) (void* vp);
 } Worker;
 
-int worker_init(Worker* worker);
+int worker_init(Worker* worker, const Fifo* job_queue);
 
 int worker_destroy(Worker* worker);
+
+int worker_start(Worker* worker);
+
+void worker_thread_function(void* arg/*Fifo of Jobs*/);
 
 #endif
