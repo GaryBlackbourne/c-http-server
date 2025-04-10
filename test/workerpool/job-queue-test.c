2d1aa493c6ff9506d87f74453162b7b7f5d2bdc2
Gergely Koloszar <gergely.koloszar@gmail.com>
2025-04-10 22:00:09 +0200

    Add fail implementations to new tests

---
+++ test/workerpool/job-queue-test.c
@@ -0,0 +1,37 @@
+#include "unity.h"
+
+#include "job-queue-test.h"
+
+
+void job_queue_init_test(void){
+    TEST_FAIL();
+}
+
+void job_queue_destroy_empty_test(void) {
+    TEST_FAIL();
+}
+
+void job_queue_destroy_non_empty_test(void) {
+    TEST_FAIL();
+}
+
+void job_queue_push_normal(void) {
+    TEST_FAIL();
+}
+
+void job_queue_push_full(void) {
+    TEST_FAIL();
+}
+
+void job_queue_pop_normal(void) {
+    TEST_FAIL();
+}
+
+void job_queue_pop_empty(void) {
+    TEST_FAIL();
+}
+
+void job_queue_pop_null_size(void) {
+    TEST_FAIL();
+}
+
