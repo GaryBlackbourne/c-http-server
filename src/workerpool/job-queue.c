3fc4a3d55c9e8f1df44c19440e6895f3693c4fdb
Gergely Koloszar <gergely.koloszar@gmail.com>
2025-04-10 21:19:09 +0200

    Fix bad error checking and follow changes in fifo api

--- src/workerpool/job-queue.c
+++ src/workerpool/job-queue.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <pthread.h>
 
+#include "configuration.h"
 #include "job-queue.h"
 #include "fifo.h"
 
@@ -12,30 +13,29 @@ int job_queue_init(JobQueue* job_queue, const Configuration* config) {
     /*
      * Try to initialize all parts. If any of them fails,
      * destroy the previously created objects.
-     */ 
+     */
 
     if (fifo_init(&job_queue->fifo, config->job_queue_length) != 0) {
         goto fifo_fail;
     }
-    if (pthread_mutex_init(&job_queue->fifo_lock, NULL)) {
+    if (pthread_mutex_init(&job_queue->fifo_lock, NULL) != 0) {
         goto fifo_mux_fail;
     }
-    // init cond var
-    if (pthread_cond_init(&job_queue->new_job_cond_v, NULL)){
+    if (pthread_cond_init(&job_queue->new_job_cond_v, NULL) != 0) {
         goto cond_var_fail;
     }
-    if (pthread_mutex_init(&job_queue->new_job_cond_mux, NULL)) {
+    if (pthread_mutex_init(&job_queue->new_job_cond_mux, NULL) != 0) {
         goto cond_var_mux_fail;
     }
 
     return 0;
 
- cond_var_mux_fail:
+cond_var_mux_fail:
     pthread_cond_destroy(&job_queue->new_job_cond_v);
- cond_var_fail:
-     pthread_mutex_destroy(&job_queue->fifo_lock);
- fifo_mux_fail:
-     destroy_fifo(&job_queue->fifo);
- fifo_fail:
+cond_var_fail:
+    pthread_mutex_destroy(&job_queue->fifo_lock);
+fifo_mux_fail:
+    fifo_destroy(&job_queue->fifo);
+fifo_fail:
     return -1;
 }
